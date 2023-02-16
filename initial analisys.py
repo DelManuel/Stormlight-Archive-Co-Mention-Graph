3b2bf16cedde9a7b4e3de05424c9be4757a00835
Manuel Delgado <manueldelgadouba@gmail.com>
2023-02-16 16:50:45 -0300

    Study of characters who shared the most chapters with another characters
    In this case I studied Kaladin.

--- initial analisys.py
+++ initial analisys.py
@@ -4,7 +4,10 @@ import pickle
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from matplotlib.ticker import MaxNLocator
+
+plt.style.use("default")
 plt.style.use("seaborn-dark")
 #%% To load the graphs
 with open('graphs/WoK.gpickle', 'rb') as f:
@@ -16,6 +19,8 @@ with open('graphs/OB.gpickle', 'rb') as f:
 with open('graphs/RoW.gpickle', 'rb') as f:
     G_row = pickle.load(f)
 
+
+list_of_graphs = [G_wok, G_wor, G_ob, G_row]
 #%% Studying central characters´s links
 
 G_wok_simple = nx.Graph(G_wok)
@@ -23,13 +28,13 @@ G_wor_simple = nx.Graph(G_wor)
 G_ob_simple = nx.Graph(G_ob)
 G_row_simple = nx.Graph(G_row)
 
-list_of_graphs = [G_wok_simple, G_wor_simple, G_ob_simple, G_row_simple]
+list_of_simple_graphs = [nx.Graph(G) for G in list_of_graphs]
 
 characters_to_analyze = ["Kaladin", "Shallan", "Dalinar", "Venli", "Szeth", "Stormfather"]
 
 edges_with_characters_by_book = [[0,0,0,0] for i in characters_to_analyze]
 
-for i, graph in enumerate(list_of_graphs):
+for i, graph in enumerate(list_of_simple_graphs):
     for edge in graph.edges():
         for character in characters_to_analyze:
             if character in edge:
@@ -51,5 +56,56 @@ ax.set_xlabel("Book number", fontsize = 18)
 ax.set_ylabel("Percentage of characters connected with", fontsize = 18)
 ax.tick_params(axis='both', which='major', labelsize=14)
 
+#plt.savefig("plots/Percentage of characters interacted.png")
+#%% Function to pass from MultiGraph to WeightedGraph
 
+def convert_to_weighted(graph):
+
+    G_weighted = nx.Graph()
+    checked_edges = []
+    edge_list = list(graph.edges())
+    for i,edge_i in (enumerate(edge_list)):
+        edge_weight = 1
+        if edge_i not in checked_edges:
+            for j in range(i+1,len(edge_list)):
+                enlace_j = edge_list[j]
+                if edge_i == enlace_j:
+                    edge_weight +=1
+        
+            checked_edges.append(edge_i)
+            if edge_i in G_weighted.edges():
+                G_weighted[edge_i[0]][edge_i[1]]["weight"] += 1
+            else:  
+                G_weighted.add_edge(edge_i[0],edge_i[1], weight = edge_weight)
+            
+    return G_weighted
+#%%
+
+weighted_graphs = [convert_to_weighted(G) for G in list_of_graphs]
+
+
+# %%
+
+most_interacted = [["",0] for i in range(len(list_of_graphs))] 
+
+for i, G in enumerate(weighted_graphs):
+
+    kaladin_list = dict(G["Kaladin"])
+    
+    for character, weight in kaladin_list.items():
+        weight = weight["weight"]
+        if weight > most_interacted[i][1]:
+            most_interacted[i] = [character,weight]
+
+most_interacted = {i[0]:i[1] for i in most_interacted}
+df = pd.DataFrame(most_interacted, index = ["Characters"])
+# %%
+fig, axs = plt.subplots(figsize = (8,4))
+
+sns.barplot(data = df, ax = axs, orient = "h")
+axs.set_title("Characters who shared the most chapters with Kaladin", fontsize = 18)
+axs.tick_params(axis = "both", labelsize = 12)
+axs.set_xlabel("Amount of shared chapters",fontsize = 14)
+
+plt.savefig("plots/Characters who shared the most chapters with Kaladin.png")
 # %%
